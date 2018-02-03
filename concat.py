c502f65a572d1311398700d0009924dac6907891
rohanricky <rohanricky0609@gmail.com>
2018-02-03 10:20:10 +0530

    lol

--- concat.py
+++ concat.py
@@ -2,8 +2,6 @@ import numpy as np
 import cv2
 import os
 
-# this two lines are for loading the videos.
-# in this case the video are named as: cut1.mp4, cut2.mp4, ..., cut15.mp4
 videofiles = [n for n in os.listdir('shit/') if n[-4:]=='.avi']
 videofiles=sorted(videofiles, key=lambda item: int(item.partition('.')[0]))
 #videofiles = sorted(videofiles, key=lambda item: int( item.partition('.')[0]))
@@ -11,7 +9,7 @@ print(videofiles)
 video_index = 0
 cap = cv2.VideoCapture(videofiles[0])
 fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-# video resolution: 1624x1234 px
+
 out = cv2.VideoWriter("video.avi",fourcc,30, (500,375),True)
 
 def concat():
@@ -28,10 +26,11 @@ def concat():
             ret, frame = cap.read()
         cv2.imshow('frame',frame)
         out.write(frame)
-        if cv2.waitKey(700) & 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-out.release()
+cap.release()
+#out.release()
 cv2.destroyAllWindows()
 
 if __name__=='__main__':

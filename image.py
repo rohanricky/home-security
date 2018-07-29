6cea91612ada51725fd7a8c18c07c046597cbd35
Rohan Kothapalli <rohanricky0609@gmail.com>
2018-07-29 12:02:41 +0530

    cleanup

--- image.py
+++
@@ -1,5 +0,0 @@
-from send_sms import sms
-
-q=sms(7085596598,'damn')
-q.send('8639138038',"Hello Buddy")
-q.logout()
--- server/comm.js
+++
@@ -1,9 +0,0 @@
-var zerorpc = require("zerorpc");
-
-var server = new zerorpc.Server({
-    hello: function(name, reply) {
-        reply(null, "Hello, " + name);
-    }
-});
-
-server.bind("tcp://0.0.0.0:4242");

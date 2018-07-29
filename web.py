25c75bdfe19c87630a46eb442542ab062fd88dd0
Rohan Kothapalli <rohanricky0609@gmail.com>
2018-07-29 21:31:27 +0530

    requirements.txt added

--- web.py
+++ web.py
@@ -5,9 +5,9 @@ from flask import Flask, request ,Response
 import configparser
 from security_cam.intrusion import secret_cam
 
-config = configparser.ConfigParser()
-config.read('config.ini')
-access_token=config['HOST']
+# config = configparser.ConfigParser()
+# config.read('config.ini')
+# access_token=config['HOST']
 
 app = Flask(__name__)
 
@@ -17,16 +17,7 @@ def home():
     try:
         secret_cam()
     except ValueError:
-        return "Fuck"
-
-# @app.route('/cool',methods=['GET','POST'])
-# def cool():
-#     if request.method == 'POST':
-#         return 'Damn it'
-#     else:
-#         return 'It damn'
-
-# send post request to my computer
+        return "Some Mistake"
 
 if __name__=='__main__':
     app.run(host='127.0.0.1', port=int(os.environ.get('PORT', 5000)), debug=True)
